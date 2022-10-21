ff571d043c9aa0964f77dbde7228b09e3e079696
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-10-21 20:45:49 -0300

    Exercicio 4 Aula 8 Sprint 3

--- Sprint 3/Exercicios Aula 8/Exercicio 3/Exercicio 3.js
+++ Sprint 3/Exercicios Aula 8/Exercicio 3/Exercicio 3.js
@@ -1,5 +1,5 @@
 let altura = document.querySelector('#altura'), peso = document.querySelector('#peso'), IMC = document.querySelector('#IMC'), botaoCalcular = document.querySelector('#calcularIMC')
-let CalcularIMC = () => {
+let CalcularIMC = (() => {
     return IMC.innerHTML = parseFloat(peso.value) / parseFloat(Math.pow(altura.value / 100, 2))
-}
+})
 botaoCalcular.addEventListener('click', CalcularIMC)
