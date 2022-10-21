84ae9bdd5c0bba3de2bc3b091a1179b09c4bbd9e
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-10-21 19:14:19 -0300

    Exercicio 2 Aula 5 Sprint 3

--- Sprint 3/Exercicios Aula 8/Exercicio 2/Exercicio 2.js
+++ Sprint 3/Exercicios Aula 8/Exercicio 2/Exercicio 2.js
@@ -1,2 +1,5 @@
 let numero1 = prompt("Informe o primeiro número"), numero2 = prompt("Informe o segundo número"), numero3 = prompt("Informe o terceiro número")
-window.log
+let maiorNumero = ((n1, n2, n3) => {
+    return (n1 > n2) ? (n1 > n3) ? n1 : n3 : (n2 > n3) ? n2 : n3
+})
+window.alert("O maior valor digitado é " + maiorNumero(numero1, numero2, numero3))
