177fc24933b4f873f47baf7955bf36d5a8de2531
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-10-25 12:30:01 -0300

    Exercicios 4 e 5 Aula 11 Sprint 3

--- Sprint 3/ExerciciosAula11/Exercicio4/main.js
+++ Sprint 3/ExerciciosAula11/Exercicio4/main.js
@@ -1,2 +1,28 @@
-let array = [10, 14, 20, 9, 16, 22]
-console.log(array.filter(vetor => vetor > 15))
+var filmes = [
+    {
+        "id": 1,
+        "title": "Die Hard",
+        "valor": 12.00,
+        "rating": 5.0
+    },
+    {
+        "id": 2,
+        "title": "Homem Aranha 1",
+        "valor": 8.00,
+        "rating": 4.0
+    },
+    {
+        "id": 3,
+        "title": "Homem de ferro",
+        "valor": 7.00,
+        "rating": 3.0
+    },
+    {
+        "id": 4,
+        "title": "Die Hard",
+        "valor": 13.00,
+        "rating": 5.0
+    }
+];
+console.log(`A. Valores com 10% de desconto: ${filmes.map(filme => filme.valor * 0.9)}\n`)
+console.log(`B. Valores maiores que 10,00: ${filmes.filter(filme => filme.valor > 10)}\nC. Média dos valores menores que 10: ${filmes.filter(filme => filme.valor < 10).reduce((acumulador, filme) => acumulador + filme.valor)}`)
--- Sprint 3/ExerciciosAula11/Exercicio5/main.js
+++ Sprint 3/ExerciciosAula11/Exercicio5/main.js
@@ -1,2 +1,27 @@
-let array = [10, 14, 20, 9, 16, 22]
-console.log(array.filter(vetor => vetor > 15))
+var filmes = [
+    {
+        "id": 1,
+        "title": "Die Hard",
+        "valor": 12.00,
+        "rating": 5.0
+    },
+    {
+        "id": 2,
+        "title": "Homem Aranha 1",
+        "valor": 8.00,
+        "rating": 4.0
+    },
+    {
+        "id": 3,
+        "title": "Homem de ferro",
+        "valor": 7.00,
+        "rating": 3.0
+    },
+    {
+        "id": 4,
+        "title": "Die Hard",
+        "valor": 13.00,
+        "rating": 5.0
+    }
+];
+console.log(filmes.filter(filme => filme.rating == 5.0).map(filme => filme.id))
