a87a734b58d5e536733d93d6f8b27500af6a3f7a
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-10-25 19:35:07 -0300

    Exercicio 8 Aula 8 Sprint 2

--- Sprint 2/ExerciciosAula8/Exercicio8/main.js
+++ Sprint 2/ExerciciosAula8/Exercicio8/main.js
@@ -1,2 +1,2 @@
-let cavalos = parseFloat(window.prompt("Digite o número de cavalos"))
-window.alert("O número de ferraduras que deve ser comprada é " + 4 * cavalos)
+let media = parseFloat(window.prompt("Digite a média de aproveitamento"))
+window.alert("O conceito do aluno é " + ((media >= 9) ? "A" : (media >= 7.5) ? "B" : (media >= 6) ? "C" : "reprovado"))
