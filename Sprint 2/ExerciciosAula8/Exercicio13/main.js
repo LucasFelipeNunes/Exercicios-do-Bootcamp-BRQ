570f13c4287b4b14c673a481f9a4494d6650f015
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-10-26 10:32:08 -0300

    Exercicio 15 Aula 8 Sprint 2

--- Sprint 2/ExerciciosAula8/Exercicio13/main.js
+++ Sprint 2/ExerciciosAula8/Exercicio13/main.js
@@ -1,6 +1,6 @@
 let valor = [], valorVezes2 = []
 for(let i = 0;i < 15;i++){
-    valor[i] = parseFloat(window.prompt("Digite a nota " + (i + 1) + " do aluno"))
+    valor[i] = parseFloat(window.prompt("Digite um valor"))
     valorVezes2[i] = valor[i] * 2
 }
 window.alert("Os valores multiplicados por 2 são: " + valorVezes2)
--- Sprint 2/ExerciciosAula8/Exercicio15/main.js
+++ Sprint 2/ExerciciosAula8/Exercicio15/main.js
@@ -1,2 +1,9 @@
-let media = parseFloat(window.prompt("Digite a média de aproveitamento"))
-window.alert("O conceito do aluno é " + ((media >= 9) ? "A" : (media >= 7.5) ? "B" : (media >= 6) ? "C" : "reprovado"))
+let valor = [], valoresMenoresQue8 = [], aux = 0
+for(let i = 0;i < 10;i++){
+    valor[i] = parseFloat(window.prompt("Digite um valor da posição " + (i + 1)))
+    if(valor[i] < 8){
+        valoresMenoresQue8[aux] = valor[i]
+        aux++
+    }
+}
+window.alert("Os valores menores que 8 digitados são: " + valoresMenoresQue8)
