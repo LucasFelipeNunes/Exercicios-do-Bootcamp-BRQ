bf967c887cd6c27b4a49dbffae40e50bf5d9f471
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-10-26 10:10:45 -0300

    Exercicio 11 Aula 8 Sprint 2

--- Sprint 2/ExerciciosAula8/Exercicio11/main.js
+++ Sprint 2/ExerciciosAula8/Exercicio11/main.js
@@ -1,2 +1,6 @@
-let idade = parseFloat(window.prompt("Digite a sua idade"))
-window.alert("Você tem " + 365 * idade + " dias de vida, ou " + 12 * idade + "meses")
+let nota = [], media = 0
+for(let i = 0;i < 10;i++){
+    nota[i] = parseFloat(window.prompt("Digite a nota " + (i + 1) + " do aluno"))
+    media += nota[i]/10
+}
+window.alert("A média aritmética das notas é " + media)
