de520be3d0b83ca65ed4d1a9a5b3043f592dcc2f
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-10-26 10:24:02 -0300

    Exercicio 14 Aula 8 Sprint 2

--- Sprint 2/ExerciciosAula8/Exercicio14/main.js
+++ Sprint 2/ExerciciosAula8/Exercicio14/main.js
@@ -1,2 +1,6 @@
-let idade = parseInt(window.prompt("Digite a idade do nadador"))
-window.alert("Este nadador se encaixa " + ((idade < 5) ? "em nenhuma categoria." : ("na categoria " + ((idade <= 10) ? "infantil" : (idade <= 18) ? "juvenil" : "adulto"))))
+let numero = [], soma = 0
+for(let i = 0;i < 10;i++){
+    numero[i] = parseFloat(window.prompt("Digite o número da posição " + (i + 1)))
+    soma += numero[i]
+}
+window.alert("A soma total dos números lidos é " + soma)
