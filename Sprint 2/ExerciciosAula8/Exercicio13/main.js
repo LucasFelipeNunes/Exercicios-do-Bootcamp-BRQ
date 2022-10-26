da12c45c47224f1568b118a0b9ca220824a39d98
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-10-26 10:18:49 -0300

    Exercicio 13 Aula 8 Sprint 2

--- Sprint 2/ExerciciosAula8/Exercicio13/main.js
+++ Sprint 2/ExerciciosAula8/Exercicio13/main.js
@@ -1,2 +1,6 @@
-let media = parseFloat(window.prompt("Digite a media do aluno"))
-window.alert((media < 5) ? "REPROVADO" : "APROVADO")
+let valor = [], valorVezes2 = []
+for(let i = 0;i < 15;i++){
+    valor[i] = parseFloat(window.prompt("Digite a nota " + (i + 1) + " do aluno"))
+    valorVezes2[i] = valor[i] * 2
+}
+window.alert("Os valores multiplicados por 2 são: " + valorVezes2)
