f9e434911d196526744523ca3390890a0c424406
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-10-28 13:13:13 -0300

    Exercicio 4 Aula 4 Sprint 2

--- Sprint 2/ExerciciosAula4/Exercicio4/main.js
+++ Sprint 2/ExerciciosAula4/Exercicio4/main.js
@@ -1,1 +1,4 @@
 let A = true, B = true, C = false
+console.log("a) (A e B) ou (A ou B) = " + ((A && B) || (A || B)))
+console.log("b) (A ou B) e (A e C) = " + ((A || B) && (A && C)))
+console.log("c) A ou C e B ou A e não B  = " + (A || C && B || A && !B))
