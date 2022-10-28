84379cf2ef74de853f21368b44c4ac772a1d429b
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-10-28 12:47:52 -0300

    Exercicio 1 Aula 4 Sprint 2

---
+++ Sprint 2/ExerciciosAula4/Exercicio4/main.js
@@ -0,0 +1,37 @@
+let X = 15, Y = 20
+console.log("a) X = " + X)
+let B = 3
+console.log("\nX = " + X + ", Y = " + Y)
+let A = 15
+B = 3
+let C = A * B
+console.log("\n\nb) C = " + C)
+B = 10, 
+C = A - B
+console.log("; A = " + A + ", B = " + B + ", C = " + C)
+X = 10
+Y = 12
+let Z = X
+Y = Z
+X = Y
+console.log("\n\nc) X = " + X + ", Y = " + Y + ", Z = " + Z)
+A = 10
+B = A + 2
+A = B + 1
+B = A + 1
+console.log("\n\nd) A = " + A)
+A = B + 1
+console.log("; A = " + A + ", B = " + B)
+X = 12
+Y = X
+Z = X + Y
+X = 20
+Y = 10
+console.log("\n\ne) X = " + X + ", Y = " + Y + ", Z = " + Z)
+A = 2
+B = 3
+A = A + B
+C = A - B
+console.log("\n\nf) C = " + C)
+B = 5
+console.log("; A = " + A + ", B = " + B + ", C = " + C)
