1c1b2c7921fb8559426965692efbe46accc0f617
Lucas Felipe da Silva Nunes <lucasfelipe.lfn@gmail.com>
2022-10-28 13:09:02 -0300

    Exercicio 3 Aula 4 Sprint 2

--- Sprint 2/ExerciciosAula4/Exercicio3/main.js
+++ Sprint 2/ExerciciosAula4/Exercicio3/main.js
@@ -1,37 +1,19 @@
-let X = 15, Y = 20
-console.log("a) X = " + X)
-let B = 3
-console.log("\nX = " + X + ", Y = " + Y)
-let A = 15
-B = 3
-let C = A * B
-console.log("\n\nb) C = " + C)
-B = 10, 
-C = A - B
-console.log("; A = " + A + ", B = " + B + ", C = " + C)
-X = 10
-Y = 12
-let Z = X
-Y = Z
-X = Y
-console.log("\n\nc) X = " + X + ", Y = " + Y + ", Z = " + Z)
-A = 10
-B = A + 2
-A = B + 1
-B = A + 1
-console.log("\n\nd) A = " + A)
-A = B + 1
-console.log("; A = " + A + ", B = " + B)
-X = 12
-Y = X
-Z = X + Y
-X = 20
-Y = 10
-console.log("\n\ne) X = " + X + ", Y = " + Y + ", Z = " + Z)
-A = 2
-B = 3
-A = A + B
-C = A - B
-console.log("\n\nf) C = " + C)
-B = 5
-console.log("; A = " + A + ", B = " + B + ", C = " + C)
+let A = [true, false, true, true, false], B = [true, true, false, true, true], C = [false, false, true, true, true]
+console.log("a)\n\tA\tB\tC\tA e B ou C")
+for(let i = 0;i < 5;i++){
+    console.log(((A) ? "\tV\t" : "\tF\t") + ((B) ? "V\t" : "F\t") + ((C) ? "V\t" : "F\t") + ((A && B || C) ? "V" : "F"))
+}
+A = [true, true, false, true]
+B = [true, false, true, true]
+C = [false, false, false, true]
+console.log("\nb)\n\tA\tB\tC\tA e B e C")
+for(let i = 0;i < 4;i++){
+    console.log(((A) ? "\tV\t" : "\tF\t") + ((B) ? "V\t" : "F\t") + ((C) ? "V\t" : "F\t") + ((A && B && C) ? "V" : "F"))
+}
+A = [true, true, true, true]
+B = [true, true, false, false]
+C = [true, false, false, true]
+console.log("\nc)\n\tA\tB\tC\tA e B ou C")
+for(let i = 0;i < 4;i++){
+    console.log(((A) ? "\tV\t" : "\tF\t") + ((B) ? "V\t" : "F\t") + ((C) ? "V\t" : "F\t") + ((A || B && C) ? "V" : "F"))
+}
